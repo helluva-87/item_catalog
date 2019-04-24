bc6623d9713ddc87d4a32f12d7f60d2cef9cddac
John Legro <jlegro@issys.com>
2019-04-24 14:15:27 -0700

    Setup Vagrant webserver on localhost:8080/catalog

--- webserver.py
+++ webserver.py
@@ -1,1 +1,46 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+clas webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/catalog"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "You have landed on the catalog page!"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print (output)
+
+
+            else:
+                self.send_error(404, "File Not Found %s" % self.path)
+
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print ("Web server running on port %s" % port)
+        server.serve_forever()
+
+
+
+
+
+    except KeyboardInterrupt:
+        print ("^C entered, stopping web server...")
+        server.socket.close()
+
+
+
+
+
+
+
+if __name__ == 'main':
+    main()
